e31fb531321c202f9eb5b554327cb9dcc9ee171e
sylvain <sylvainc@163.com>
2018-08-13 11:19:04 +0800

    scale size * 2

--- Love_files/love.js
+++ Love_files/love.js
@@ -57,7 +57,7 @@
         // y = 13 cos t - 5 cos 2t - 2 cos 3t - cos 4t
         // http://www.wolframalpha.com/input/?i=x+%3D+16+sin%5E3+t%2C+y+%3D+(13+cos+t+-+5+cos+2t+-+2+cos+3t+-+cos+4t)
         var points = [], x, y, t;
-        for (var i = 10; i < 30; i += 0.2) {
+        for (var i = 10; i < 30; i += 0.1) {
             t = i / Math.PI;
             x = 16 * Math.pow(Math.sin(t), 3);
             y = 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
